a42145eab5474196e5a958f74d41d8a684e49883
Victor Zamora <victorzamoragutierrez@gmail.com>
2016-05-30 10:39:55 -0500

    Modifica la enumeración de tipo de Celda. La puse de manera que el tipo de celda dependa de la probabilidad de que el robot esté ahí. Esto es para que las celdas se puedan colorear de diferente color en base a la probabilidad. No sé qué tan buena idea sea y si es necesario, pienso cambiarlo en el futuro.

--- Proyecto/src/TipoCelda.java
+++ Proyecto/src/TipoCelda.java
@@ -1,6 +1,8 @@
 /* Enumeración para representar el tipo de una celda */
 public enum TipoCelda {
-    ACTUAL, /* Si el robot está parado en la celda */
     OBSTACULO, /* Por ahora, se deja como obstáculo. En el futuro, se puede extender a diferentes tipos de obstáculo, como mesa y silla. */
-    VACIA /* Si no hay nada en la celda */
+    /* La siguientes 3 probabilidades representan una estimación de la probabilidad de que el robot esté en la celda */
+    PROBABILIDAD_ALTA,
+    PROBABILIDAD_MEDIA,
+    PROBABILIDAD_BAJA
 }
