1656607b9db43a7d527d5f60f8c5d9ad6c3c26d2
Victor Zamora <victorzamoragutierrez@gmail.com>
2016-06-11 13:41:24 -0500

    La clase Celda ya almacena distancias y así

--- Proyecto/src/Celda.java
+++ Proyecto/src/Celda.java
@@ -1,12 +1,26 @@
 /* Representa a una Celda de nuestro mundo */
 public class Celda{
     private TipoCelda tipo; /* El tipo de la Celda (se usa para dibujarla en Processing */
-
-    /* Constructor. Las celdas se inicializan con probabilidad media, pues al principio todas las celdas que no sean obstáculos tienen la misma probabilidad de que el robot esté ahí. */
-    public Celda(){
-	this.tipo = TipoCelda.PROBABILIDAD_MEDIA;
-    }
+    private double creencia; /* La probabilidad de que el robot se encuentre en esta Celda */
+    /* Aquí vienen las distancias a obstáculos. Las haré enteras porque podemos considerar la distancia en
+       diagonal como 14 como en la práctica de A* para simplificar(?) */
+    private int[] distanciaObstaculo; /* Las distancias a los obstáculos en las diferentes direcciones */
+    /* Las 8 direcciones */
+    public static final int N = 0; /* Norte */
+    public static final int NE = 1; /* Noreste */
+    public static final int E = 2; /* Este */
+    public static final int SE = 3; /* Sureste */
+    public static final int S = 4; /* Sur */
+    public static final int SO = 5; /* Suroeste */
+    public static final int O = 6; /* Oeste */
+    public static final int NO = 7; /* Noroeste */
     
+    /* Constructor. Define el número de direcciones como 8 */
+    public Celda(){
+	this.tipo = TipoCelda.OBSTACULO;
+	distanciaObstaculo = new int[8];
+    }
+
     /* Regresa el tipo de la Celda */
     public TipoCelda getTipo(){
 	return this.tipo;
@@ -15,6 +29,27 @@ public class Celda{
     /* Asigna al tipo como 'nuevoTipo' */
     public void setTipo(TipoCelda nuevoTipo){
 	this.tipo = nuevoTipo;
+    }
+
+    /* Regresa la creencia de la Celda */
+    public double getCreencia(){
+	return this.creencia;
+    }
+
+    /* Asigna a la creencia como 'creencia' */
+    public void setCreencia(double creencia){
+	this.creencia = creencia;
+    }
+
+    /* Regresa el arreglo de las distancias a los obstáculos en todas las
+       direcciones */
+    public int[] getDistanciaObstaculo(){
+	return this.distanciaObstaculo;
+    }
+    
+    /* Pone el arreglo de distancias como 'nuevasDistancias' */
+    public void setDistanciaObstaculo(int[] nuevasDistancias){
+	thiis.distanciaObstaculo = nuevasDistancias;
     }
     
 }
